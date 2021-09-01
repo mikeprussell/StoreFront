de5e2bcab49088312ab1aba739c74943a6b101f9
Michael Russell <mikeprussell@outlook.com>
2021-09-01 08:19:18 -0500

    Commit template integration

--- StoreFront/StoreFront.UI.MVC/Controllers/HomeController.cs
+++ StoreFront/StoreFront.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using StoreFront.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Configuration;
+using System.Web.Mvc;
 
 namespace StoreFront.UI.MVC.Controllers
 {
@@ -22,9 +26,45 @@ namespace StoreFront.UI.MVC.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            string body = $"Message received from MVC2 Secured. From {cvm.Name} - Subject {cvm.Subject}<br />Message:" +
+                $"{cvm.Message}";
+
+            string emailUser = WebConfigurationManager.AppSettings["EmailUser"];
+            string emailToAddress = WebConfigurationManager.AppSettings["EmailToAddress"];
+            string emailServer = WebConfigurationManager.AppSettings["EmailServer"];
+            string emailPassword = WebConfigurationManager.AppSettings["EmailPassword"];
+
+
+            MailMessage msg = new MailMessage(emailUser, emailToAddress, "Message from your contact form", body);
+
+            msg.CC.Add("mikeprussell@hotmail.com");
+            msg.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient(emailServer);
+            client.Credentials = new NetworkCredential(emailUser, emailPassword);
+
+            try
+            {
+                client.Send(msg);
+                return View("EmailConfirmation", cvm);
+
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.ErrorMessage = "Oops. Something went wrong - try again or contact an administrator.";
+
+            }
+
+            return View();
+
+        }
+
     }
+
 }
